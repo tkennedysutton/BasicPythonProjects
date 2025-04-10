ae63e76d9f9167eb7eea719af8215edca9b1371a
tkennedysutton <tristan.c.kennedy@gmail.com>
2025-04-10 08:39:47 +0100

    variables

---
+++ Day1_notes.py
@@ -0,0 +1,16 @@
+#Variables
+name = "Tristan"                        #string
+Steaks_eaten = 100                      #int - whole number
+time_to_run_to_coffee_machine = 6.234   #float - can have decimals
+Currently_has_coffee = False            #bool - true or false values
+
+#Variables in ML world
+dataset_name = "COFFEE"
+batch_size = 64
+learning_rate = 0.001
+is_cuda_available = True
+
+#Messing with variables
+print(name)
+if name == "Tristan":                   # == used to compare values in python
+    print("True")
